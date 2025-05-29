06ad079e272103f3666406f1fadb9b33d4c7838b
Vagarth Pandey <vagarth419@gmail.com>
2025-05-29 22:04:55 +0530

    Update page.tsx

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -6,7 +6,9 @@ const user = await prisma.user.findFirst();
   
   return (
     <div>
+      email: 
       <h1>{user?.email}</h1>
+      password: 
       <h1>{user?.password}</h1>
     </div>
   );
