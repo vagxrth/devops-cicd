08094f1ffb9cbca8ed831bdeffa54eacb8bbe6ea
Vagarth <vagarth419@gmail.com>
2025-05-30 15:11:52 +0530

    fix(page): update Home component to display hardcoded email and password for testing purposes

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,8 +2,8 @@ export default async function Home() {
   
   return (
     <div>
-      <h1>email:</h1> 
-      <h1>password: </h1>
+      <h1>email: vagxrth@gmail.com</h1> 
+      <h1>password: whothehellisthis</h1>
     </div>
   );
 }
