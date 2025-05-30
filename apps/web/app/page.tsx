2c6c50665e8d1efc5754d1a0248a1fcf454b1813
Vagarth <vagarth419@gmail.com>
2025-05-30 16:22:58 +0530

    fix(page): correct hardcoded email in Home component for accuracy

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,7 +2,7 @@ export default async function Home() {
   
   return (
     <div>
-      <h1>email: vagarthpandey@gmail.com</h1> 
+      <h1>email: vagxrth@gmail.com</h1> 
       <h1>password: whothehellisthis</h1>
     </div>
   );
